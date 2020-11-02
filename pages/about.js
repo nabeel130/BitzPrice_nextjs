a537612ae1c29e7e30f7b7caf7d530f8d7ea90c1
Mohammed Nabeel km <nabeelkmvkd07@gmail.com>
2020-11-02 12:12:42 +0530

    initial commit , UI set up is completed

---
+++ pages/about.js
@@ -0,0 +1,11 @@
+import Layout from '../components/Layout';
+
+const About = () => (
+    <Layout>
+    <div>
+        <h1>About the BitzPrice</h1>
+        <p>Application to view bitzprices</p>
+    </div>
+    </Layout>
+);
+export default About;
